e27740571e1a3babb4b43047d932cc63f7a919ff
Craig Broadman <CBroadman@ipipeline.com>
2021-05-12 15:09:14 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function run(): Promise<void> {
       return;
     }
 
-    core.debug(`pr description: ${pr}`);
+    core.debug(`pr description: ${pr.body}`);
 
     // Get input parameters.
     const githubToken = core.getInput('repo-token');
