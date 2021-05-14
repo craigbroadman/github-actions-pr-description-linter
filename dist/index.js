0ceb7b7648a820d5d08de90c20400d0bde8d6152
Craig Broadman <CBroadman@ipipeline.com>
2021-05-14 11:09:00 +0100

    merge

--- dist/index.js
+++ dist/index.js
@@ -111,7 +111,7 @@ function dismissReview(pullRequest) {
                     repo: pullRequest.repo,
                     pull_number: pullRequest.number,
                     review_id: review.id,
-                    message: 'All actions resolved, you\'re good to go ✅'
+                    message: "All actions resolved, you're good to go ✅"
                 });
             }
         }
